5ee352f5f8793d623e03ee3e79ea70e8ad911ccf
grusnny <37426267+grusnny@users.noreply.github.com>
2021-02-22 10:49:11 -0500

    Add to Models ConnectionStrings

---
+++ Models/ConnectionStrings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothesstoreProductsAPI.Models
+{
+    public class ConnectionStrings
+    {
+        public string MySQL { get; set; }
+
+    }
+}
